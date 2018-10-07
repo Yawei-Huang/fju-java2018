c96218bd1eb99e48f48402962f2c6d3094c5c025
Angela <Angela@172.20.10.3>
2018-10-07 14:20:01 +0800

    initial

---
+++ myproject/src/com/tom/Hello.java
@@ -0,0 +1,18 @@
+package com.tom;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		int age = 18;
+		System.out.println(age);
+		float height = 1.8f;
+		String name = "Angela";
+		
+		System.out.println(height);
+		System.out.println(name);
+		
+	}
+
+}
